b715025ff11cdf54057c6ff10a225198723eb055
Whoop <andrew.ridenour7@gmail.com>
2023-04-25 20:21:03 -0400

    Created HighCommand and Commander classes, Implemented Force Disposition Strategies

--- dcsgroundai.lua
+++ dcsgroundai.lua
@@ -21,6 +21,169 @@ function table.invert(table)
 	return s
 end
 
+--
+-- HIGHCOMMAND Class Definition
+--
+
+function newHIGHCOMMAND (PREFIX)
+
+	local _PREFIX = PREFIX
+	local _commanders = {}
+	local _targetZones = {}
+	local _zones = getAllZonesWithPrefix(PREFIX)
+
+	--- Select zones to assign to commanders
+	-- @param numTargetZones 
+	local selectTargetZones = function (numTargetZones)
+		zoneList = mist.utils.deepCopy(_zones)
+		_targetZones = {}
+		
+		for i = 1, numTargetZones do
+			table.insert(_targetZones, table.remove(zoneList, math.random(#zoneList)))
+		end
+	end
+	
+	--- Generate commanders based on user-provided commanderConfig
+	-- @param commanderConfig - TBD
+	local assignForceStructure = function (commanderConfig)
+			
+		-- Reset commander list
+		_commanders = {}
+
+		-- Get forces in battlefield with user-defined prefix
+		for _, g in pairs(mist.DBs.groupsByName) do
+			-- Parse group object name into prefix, commander's name, and group's name
+			-- [PREFIX]_[COMMANDERNAME]-[GROUPNAME]
+			local gPrefix, cName, gName = string.match(g.groupName, "(.*)_(.*)-(.*)")
+
+			-- Validate that the group contains the correct PREFIX and has a commander that isn't ignored
+			if (gPrefix == _PREFIX) then
+			
+				if (commanderConfig[cName] ~= nil) then
+					--	Check if commander object already exists. If not, create it.
+					if (_commanders[cName] == nil) then
+						_commanders[cName] = newCOMMANDER(cName, commanderConfig[cName], g.coalition)
+					end
+					
+					-- 	Add unit/group to commander object's "forces" table.
+					_commanders[cName].addForces(g)
+				end
+			end
+		end
+	end
+	
+	-- Returns list of commander objects in coalition blue
+	local getCoalitionCommanders = function (coalition)
+		local list = {}
+		for k, v in pairs(_commanders) do
+			if v.getCoalition() == coalition then
+				list[#list+1] = v
+			end
+		end
+		return list
+	end
+	
+	local assignMissions = function (coalition, numZonesPerCommander)
+		local assignableZones = mist.utils.deepCopy(_targetZones)
+		local coalitionCommanders = getCoalitionCommanders(coalition)
+		
+		-- Assign Missions
+		-- For all coalition commanders, assign numZones zones from targetZones
+		for _, cObj in pairs(coalitionCommanders) do
+
+			-- Set reference point to the location of a randomly selected group in this commander's forces
+            local referencePoint = mist.getGroupPoints(cObj.getForces()[1].groupName)[1]
+
+			local closestZones = getNClosestZones(assignableZones, numZonesPerCommander, referencePoint)
+
+			-- Remove closest zones from assignableZones so they are not assigned to another Commander
+            local removeZones = {}
+			for i = 1, #assignableZones do
+				for j = 1, #closestZones do
+                    if assignableZones[i].name == closestZones[j] then
+                        removeZones[#removeZones+1] = i
+                    end
+				end
+			end
+           
+            for i = 1, #removeZones  do
+               table.remove(assignableZones, removeZones[#removeZones + 1 - i])
+            end
+		
+		
+			-- Assign missions to commander
+			cObj.assignZones(closestZones)
+		end
+	end
+	
+	-- Command all commanders to execute orders
+	local executeMissions = function ()
+		for _, commander in pairs(_commanders) do
+			commander.executeOrders()
+		end
+	end
+
+	return {
+		getIgnoredCommanders = function () return _ignoredCommanders end,
+		getTargetZones = function () return _targetZones end,
+		getCommanders = function () return _commanders end,
+		getBlueCommanders = function () return getCoalitionCommanders("blue") end,
+		getRedCommanders = function () return getCoalitionCommanders("red") end,
+		selectTargetZones = selectTargetZones,
+		assignForceStructure = assignForceStructure,
+		assignMissions = assignMissions,
+		executeMissions = executeMissions
+	}
+end
+
+--
+-- End HIGHCOMMAND Class Definition
+--
+
+--
+-- COMMANDER Class Definition
+--
+
+function newCOMMANDER(id, strategy, coalition)
+	local _id = id
+	local _strategy = strategy
+	local _coalition = coalition
+	local forces = {}
+	local assignedZones = {}
+	
+
+	local addForces = function(group)
+		forces[#forces + 1] = group
+	end
+	
+	local assignZones = function (zones)
+		assignedZones = zones
+	end
+	
+	local executeOrders = function()
+		-- Generate plan
+		if (_strategy == nil or not type(_strategy) == "function") then
+			StrategyDefault(assignedZones, forces)
+		else
+			_strategy(assignedZones, forces)
+		end
+	end
+
+	return {
+		getId = function () return _id end,
+		getCoalition = function () return _coalition end,
+		getForces = function () return forces end,
+		getAssignedZones = function () return assignedZones end,
+		addForces = addForces,
+		assignZones = assignZones,
+		executeOrders = executeOrders
+	}
+end
+
+--
+-- End COMMANDER Class Definition
+--
+
 function getNClosestZones(zoneList, numZones, referencePoint)
 	local closestZones = {}
 	local distances = {}
@@ -45,55 +208,6 @@ function getNClosestZones(zoneList, numZones, referencePoint)
 	end
 
 	return closestZones
-end
-
-function assignMissions(targetZones, commanderList, numZones)
-	local assignableZones = {}
-	
-	-- Initialize assignable zones
-	for key, zone in pairs(targetZones) do
-		table.insert(assignableZones, zone)
-	end
-	
-	-- Assign Missions
-	-- For all coalition commanders, assign numZones zones from targetZones
-	for cName, cObj in pairs(commanderList) do
-
-		-- Set reference point to the location of a randomly selected group in this commander's forces
-		local referencePoint = mist.getGroupPoints(commanderList[cName]["forces"][1])[1]
-		 
-		local closestZones = getNClosestZones(assignableZones, numZones, referencePoint)
-		
-		-- Remove closest zones from assignableZones so they are not assigned to another Commander
-		for i = 1, #assignableZones do
-			for j = 1, #closestZones do
-				if assignableZones[i] == closestZones[j] then
-					table.remove(assignableZones, i)
-					i = i - 1
-				end
-			end
-		end
-	
-		-- Assign missions to commander
-		commanderList[cName]["assignedZones"] = closestZones
-	end
-	
-	return commanderList
-end
-
-function zoneSelector(zoneList, numZones, removeFromList)
-	-- randomly choose zones from a list of zones
-	-- if removeFromList is true, chosen zones will be removed from the list (original object is mutated)
-	local targetZones = {}
-	for i = 1,numZones do
-		if removeFromList then
-			table.insert(targetZones, table.remove(zoneList, math.random(#zoneList)))
-		else
-			table.insert(targetZones, zoneList[math.random(#zoneList)])
-		end
-		
-	end
-	return targetZones
 end
 
 function getAllZonesWithPrefix(prefix)
@@ -125,63 +239,46 @@ function getAllZonesWithPrefix(prefix)
 	return list
 end
 
-function generateCommander()
-
-	-- Commander Strategy Enumerations
-	-- DEFAULT: 
-	
-	-- TODO: Commander strategy enumerations, and 
-	-- 		add logic to assign a strategy to commanders
-	--	
-	--	Need to consider how commanders can persist 
-	--		between missions.
-	
-	return {
-		["strategy"] = "DEFAULT",
-		["forces"] = {},
-		["assignedZones"] = {}
-	}
-end
-
-function getForceStructure(prefix, ignoredCommanders) 
-	-- Returns a list of commanders and their forces, grouped by coalition
-	-- {
-	--		["Commander1"] = {
-	--			["strategy"] = "STRATEGY_NAME",
-	--			["forces"] = {
-	--				"Unit/Group 1 name"
-	--				"Unit/Group 2 name"
-	--			}
-	--		}
-	-- }
-
-	-- Get forces in battlefield with user-defined prefix
-	local commanderList = {
-		["blue"] = {},
-		["red"] = {},
-	}
-	for _, g in pairs(mist.DBs.groupsByName) do
-		-- Parse group object name into prefix, commander's name, and group's name
-		-- [PREFIX]_[COMMANDERNAME]_[GROUPNAME]
-		local groupPrefix, commanderName, groupName = string.match(g.groupName, "(.*)_(.*)-(.*)")
-
-		-- Validate that the group contains the correct _PREFIX and has a commander that isn't ignored
-		if (groupPrefix == prefix) then
-		
-			if (not table.contains(ignoredCommanders, commanderName)) then
-				--	Check if commander object already exists. If not, create it.
-				if (commanderList[g.coalition][commanderName] == nil) then
-					commanderList[g.coalition][commanderName] = generateCommander()
-				end
-				
-				-- 	Add unit/group to commander object's "forces" table.
-				table.insert(commanderList[g.coalition][commanderName]["forces"], g.groupName)
-			end
-		end
+function getZoneNames(zones)
+	local zoneList = {}
+	for k, v in pairs(zones) do
+		zoneList[#zoneList+1] = v.name
 	end
-	
-	return commanderList
+	return zoneList
 end
+
+--
+-- STRATEGIES
+--
+
+function StrategyDefault(zones, groups)
+	for i = 1, #groups do
+		local group = groups[i].groupName
+		local zone = zones[math.random(#zones)]
+		mist.groupToRandomZone(group, zone, nil, nil, 50, true) 
+	end
+end
+
+function StrategyAggressive(zones, groups)
+	for i = 1, #groups do
+		mist.groupToRandomZone(groups[i].groupName, zones[1], nil, nil, 50, true)
+	end
+end
+
+function StrategyWaves(zones, groups)
+	local time = 0
+	
+	for k, v in pairs(groups) do
+		-- Send waves every X seconds
+		mist.scheduleFunction(mist.groupToRandomZone, {v.name, getZoneNames(zones), nil, nil, 50, true}, timer.getTime() + 10 + time)
+		
+		time = time + 15
+	end
+end
+
+--
+-- END STRATEGIES
+--
 
 
 -- *********************************************************
@@ -190,54 +287,53 @@ end
 -- 0. Setup
 -- *********************************************************
 
--- Mission Editor inputs
-
-local _NUM_TARGET_ZONES = 5
-local _IGNORED_COMMANDERS = {'A'}
+-- *** Mission Editor inputs ***
 
 -- mission editors may change the prefix below to whatever they wish to use in the mission.
 -- if the zone name prefix is followed by an R (e.g. 'GAZR-1), the zone will be flagged as Red
 -- if the zone name prefix is followed by an B (e.g. 'GAZB-1), the zone will be flagged as Blue
 -- if the zone name prefix is not followed by a B or and R, the zone will be flagged as Neutral
 local _PREFIX = 'GAZ'
-local _NUM_ZONES_PER_COMMANDER = 2
+
+local _NUM_TARGET_ZONES = 5
+local _IGNORED_COMMANDERS = {'A'}
+local _NUM_ZONES_PER_BLUE_COMMANDER = 2
+local _NUM_ZONES_PER_RED_COMMANDER = 1
+
+local _COMMANDER_CONFIG = {
+	["B"] = StrategyDefault,
+	["C"] = StrategyDefault,
+	["D"] = StrategyDefault
+}
+
+-- *** Instanstiate High Command
+local highCommand = newHIGHCOMMAND(_PREFIX)
 
 -- *********************************************************
 -- 1. Zone Control
 -- *********************************************************
-local zoneList = {}
-zoneList = getAllZonesWithPrefix(_PREFIX)
-
-local targetZones = {}
-targetZones = zoneSelector(zoneList, _NUM_TARGET_ZONES, false)
+highCommand.selectTargetZones(_NUM_TARGET_ZONES)
 
 
 -- *********************************************************
 -- 3b.  Force Structure
 -- *********************************************************
-local commanderForces = getForceStructure(_PREFIX, _IGNORED_COMMANDERS)
+highCommand.assignForceStructure(_COMMANDER_CONFIG)
+
 
 -- *********************************************************
 -- 2.  Higher Order Command: Assign the commanders missions 
 -- *********************************************************
-assignMissions(targetZones, commanderForces["blue"], _NUM_ZONES_PER_COMMANDER)
-assignMissions(targetZones, commanderForces["red"], _NUM_ZONES_PER_COMMANDER)
+highCommand.assignMissions("blue", _NUM_ZONES_PER_BLUE_COMMANDER)
+highCommand.assignMissions("red", _NUM_ZONES_PER_RED_COMMANDER)
+
 
 -- *********************************************************
 -- 4.  Force Disposition
 -- *********************************************************
--- TBD
+-- (Currently assigned in step 3b.
 
 -- *********************************************************
 -- 5.  Movement Orders: Send units to waypoints based on commanders' missions
 -- *********************************************************
-for _, coalition in pairs({"blue", "red"}) do
-	for commander, __ in pairs (commanderForces[coalition]) do
-		for i = 1, #commanderForces[coalition][commander]["forces"] do
-			local assignedZones = commanderForces[coalition][commander]["assignedZones"]
-			local randomZone = assignedZones[math.random(#assignedZones)]
-			
-			mist.groupToRandomZone(commanderForces[coalition][commander]["forces"][i], randomZone, nil, nil, 50, true)
-		end
-	end
-end
+highCommand.executeMissions()

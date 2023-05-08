dc65d4e5b95cfe524586297f1d9cad525c091b05
Whoop <andrew.ridenour7@gmail.com>
2023-05-08 19:30:22 -0400

    Fixed variable naming bug

--- dcsgroundai.lua
+++ dcsgroundai.lua
@@ -298,14 +298,14 @@ local _PREFIX = 'GAZ'
 local _NUM_TARGET_ZONES = 5
 local _IGNORED_COMMANDERS = {'A'}
 local _NUM_ZONES_PER_BLUE_COMMANDER = 2
-local _COMMANDER_CONFIG = {
+local _BLUE_COMMANDER_CONFIG = {
 	["B"] = StrategyDefault,
 	["C"] = StrategyDefault,
 	["D"] = StrategyDefault
 }
 
 local _NUM_ZONES_PER_RED_COMMANDER = 1
-local _COMMANDER_CONFIG = {
+local _RED_COMMANDER_CONFIG = {
 	["C"] = StrategyDefault,
 }
 

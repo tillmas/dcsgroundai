c3a0ba85c7795cfa9a4fd87014a072f2163014bd
Whoop <andrew.ridenour7@gmail.com>
2023-05-08 19:36:10 -0400

    Fixed user input file

--- dcsgroundai_INPUTS.lua
+++ dcsgroundai_INPUTS.lua
@@ -6,7 +6,6 @@
 local _PREFIX = 'GAZ'
 
 local _NUM_TARGET_ZONES = 5
-local _IGNORED_COMMANDERS = {'A'}
 local _NUM_ZONES_PER_BLUE_COMMANDER = 2
 local _BLUE_COMMANDER_CONFIG = {
 	["B"] = StrategyDefault,
@@ -16,5 +15,5 @@ local _BLUE_COMMANDER_CONFIG = {
 
 local _NUM_ZONES_PER_RED_COMMANDER = 1
 local _RED_COMMANDER_CONFIG = {
-	["C"] = StrategyDefault,
+	["B"] = StrategyDefault,
 }

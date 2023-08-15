f833c0597a790766561d6dfc2494b279b5dddddc
tillmas <mtillman76@gmail.com>
2023-08-15 09:37:50 -0400

    Update dcsgroundai_INPUTS.lua (#17)
    Modified commander strategies to be of the form "StrategyDefault" (added quotes).
    
    Dropped last comma in RED Commander list.

--- dcsgroundai_INPUTS.lua
+++ dcsgroundai_INPUTS.lua
@@ -8,14 +8,14 @@ _PREFIX = 'GAZ'
 _NUM_TARGET_ZONES = 5
 _NUM_ZONES_PER_BLUE_COMMANDER = 2
 _BLUE_COMMANDER_CONFIG = {
-	["B"] = StrategyDefault,
-	["C"] = StrategyDefault,
-	["D"] = StrategyDefault
+	["B"] = "StrategyDefault",
+	["C"] = "StrategyDefault",
+	["D"] = "StrategyDefault"
 }
 
 _NUM_ZONES_PER_RED_COMMANDER = 1
 _RED_COMMANDER_CONFIG = {
-	["E"] = StrategyDefault,
-	["F"] = StrategyDefault,
-	["G"] = StrategyDefault,
+	["E"] = "StrategyDefault",
+	["F"] = "StrategyDefault",
+	["G"] = "StrategyDefault"
 }
